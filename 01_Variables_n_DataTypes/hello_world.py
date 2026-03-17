ab1a68f55aaac3de8054d3f1a0c81fdbeeefd1e7
SameekD <sameek.123@gmail.com>
2026-03-17 12:07:57 +0000

    new code lines added

--- 01_Variables_n_DataTypes/hello_world.py
+++ 01_Variables_n_DataTypes/hello_world.py
@@ -1,1 +1,15 @@
-print("Hello Sameek!!")
+print ( "Hello Sameek!!" )
+print ( 5000 + 30 * 8 - 326   )
+print ( 5 ** 2 )
+print ( round(40 / 7, 2 ) )
+print ( 40 // 7 )
+width = 20
+height = 5 * 9
+print ( width * height )
+n = "Missing Value"
+print(n)
+tax = 12.5 / 100
+price = 100.50
+print ( price * tax )
+print( price + price * tax )
+print ( round( price * tax, 2 ) )
