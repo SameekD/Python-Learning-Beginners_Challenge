b2f7315bd9f2b2062f17b9a4adc9af9540a0811d
SameekD <sameek.123@gmail.com>
2026-04-01 10:57:21 +0000

    new code

---
+++ 02_Conditional_Statements/5divisible.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter a number to check :"))
+if num1 % 5 == 0 :
+    print(f"Entered number {num1} is divisible by 5.")
+else :
+    print(f"Entered number {num1} is not divisible by 5.")
---
+++ 02_Conditional_Statements/greatestof3.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number to compare :"))
+num2 = int(input("Enter second number to compare :"))
+num3 = int(input("Enter third number to compare :"))
+
+if num1 > num2 & num1 > num3 :
+    print(f"{num1} is greater than both {num2} and {num3}.")
+elif num1 < num2 & num1 > num3 :
+    if num2 > num3 :
+        print(f"{num2} is greater than both {num1} and {num3}.")
+    else:
+        print(f"{num3} is greater than both {num1} and {num2}.")
+else:
+    print(f"{num3} is greater than both {num1} and {num2}.")
