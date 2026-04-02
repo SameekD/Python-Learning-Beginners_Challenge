984467a61aad39c5575dc71d9cb48bfed133decf
SameekD <sameek.123@gmail.com>
2026-04-02 09:46:44 +0000

    updates

---
+++ 02_Conditional_Statements/userpass.py
@@ -0,0 +1,8 @@
+admin = "Admin"
+passwrd = "12345"
+input_user = input ("Please enter username : ")
+input_pass = input ("Please enter password : ")
+if (input_user == admin and input_pass == passwrd) :
+    print("You're welcome!!")
+else :
+    print("Entered username or passwrod does not match.")
